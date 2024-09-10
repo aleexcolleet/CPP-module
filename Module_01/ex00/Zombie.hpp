ed21a983c7e13158b88f02d28915cf73da395904
Alex Collet Martínez <aleexcolleet@AlexCollet-MacBook-Pro.local>
2024-09-10 21:11:33 +0200

    learning module01

---
+++ Module_01/ex00/Zombie.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie{
+	private:
+		std::string _name;
+
+	public:
+		zombie();
+		~zombie();
+		void	announce(void);
+};
+
+#endif
---
+++ Module_01/ex00/newZombie.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+class newZombie{
+	public:
+		Zombie* newZombie(std::string name) : _name(name){
+			char*
+		}
+
+};
